418a4af6e8134fcf3931e09a9e459a4329ff3764
Luca Montanari <luca.montanari@libero.it>
2017-10-09 23:28:48 +0200

    Add PATCH /todos/:id

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,8 +2,8 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+//mongoose.connect('mongodb://localhost:27017/TodoApp');
 
-//mongoose.connect('mongodb://admin:admin@ds113435.mlab.com:13435/node-todo-api');
+mongoose.connect('mongodb://admin:admin@ds113435.mlab.com:13435/node-todo-api');
 
 module.exports = {mongoose};
