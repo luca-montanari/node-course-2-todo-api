829bf97987ee763db7f3ec49ad9e7d76bd9e9962
Luca Montanari <luca.montanari@libero.it>
2017-10-10 22:34:04 +0200

    Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,6 +4,9 @@ mongoose.Promise = global.Promise;
 
 //mongoose.connect('mongodb://localhost:27017/TodoApp');
 
-mongoose.connect('mongodb://admin:admin@ds113435.mlab.com:13435/node-todo-api');
+//mongoose.connect('mongodb://admin:admin@ds113435.mlab.com:13435/node-todo-api');
+
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
+
