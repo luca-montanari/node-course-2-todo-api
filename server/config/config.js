a74268753ffc58c4a206d7890643b02c60c34266
Luca Montanari <luca.montanari@libero.it>
2017-10-16 22:08:15 +0200

    test1

--- server/config/config.js
+++ server/config/config.js
@@ -1,14 +1,12 @@
 var env = process.env.NODE_ENV || 'development';
 
-console.log('dddddddddd', env);
-
-var config = require('./config.json');
-
-var envConfig = config[env];
-
-Object.keys(envConfig).forEach((key) => {
-    if (!(env === 'production' && key === 'PORT'))
-    {
-        process.env[key] = envConfig[key];
-    }    
-});
+if ((env === 'development') || (env === 'test')) {
+    var config = require('./config.json');
+    var envConfig = config[env];
+    Object.keys(envConfig).forEach((key) => {
+        if (!(env === 'production' && key === 'PORT'))
+        {
+            process.env[key] = envConfig[key];
+        }    
+    });
+}
